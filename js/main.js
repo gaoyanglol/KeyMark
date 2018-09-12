500504b5fd214aebeb68c404c8cd7cfc7ad50516
gaoyanglol <gaoyanglol@hotmail.com>
2018-09-12 08:52:45 +0800

    css beautify

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var keys = [['q','w','e','r','t','y','u','i','o','p'], ['a','s','d','f','g','h','j','k','l'], ['z','x','c','v','b','n','m']]
 
-var hash = {'q': 'qq.com', 'w': 'weibo.com','e': 'ele.me', 'r': 'renren.com', 't': 'tianya.com', 'y': 'youku.com', 'u': 'uc.com', 'i': 'iqiyi.com', 'o': 'opera.com', 'p': 'pinterest.com', 'a': 'acfun.com', 's': 'sohu.com', 'z': 'zhihu.com', 'm': 'mozilla.com'}
+var hash = {'q': 'qq.com', 'w': 'weibo.com','e': 'ele.me', 'r': 'mozilla.com', 't': 'opera.com', 'y': 'zhihu.com','u': 'tianya.com'}
 
 var hashInLocalStorage = JSON.parse(localStorage.getItem('zzz') || 'null')
 if(hashInLocalStorage) {
@@ -16,14 +16,26 @@ while (i < keys.length) {
     kbd = document.createElement('kbd')
     kbd.textContent = keys[i][j]
     button = document.createElement('button')
-    button.textContent = '编辑'
+    button.textContent = 'E'
     button.id = keys[i][j]
+    img = document.createElement("img")
+    img.className = "fav-ico"
+    if (hash[keys[i][j]]) {
+      img.src = "https://www." + hash[keys[i][j]] + "/favicon.ico"
+    } else {
+      img.src = "./images/holder.png"
+    }
+    img.onerror = function(event) {
+      event.target.src = "./images/holder.png"
+    }
     button.onclick = function(xzkjcnxlkcjlk) {
       hashKey = xzkjcnxlkcjlk.target.id
+      img2 = xzkjcnxlkcjlk.target.previousSibling
       hash[hashKey] = prompt('请输入要修改的网址')
       localStorage.setItem('zzz', JSON.stringify(hash))
       console.log(hash)
     }
+    kbd.appendChild(img)
     kbd.appendChild(button)
     div1.appendChild(kbd)
     j = j + 1
