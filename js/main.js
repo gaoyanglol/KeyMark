f3c22a73fee8785ac766b46763cfde5cbfc1643f
gaoyanglol <gaoyanglol@hotmail.com>
2018-09-17 15:12:50 +0800

    code refactor

--- js/main.js
+++ js/main.js
@@ -1,51 +1,71 @@
-var keys = [['q','w','e','r','t','y','u','i','o','p'], ['a','s','d','f','g','h','j','k','l'], ['z','x','c','v','b','n','m']]
+// 1.初始化数据
+var keys = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'], ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'], ['z', 'x', 'c', 'v', 'b', 'n', 'm']]
 
-var hash = {'q': 'qq.com', 'w': 'weibo.com','e': 'ele.me', 'r': 'mozilla.com', 't': 'opera.com', 'y': 'zhihu.com','u': 'tianya.com'}
-
-var hashInLocalStorage = JSON.parse(localStorage.getItem('zzz') || 'null')
-if(hashInLocalStorage) {
+var hash = { 'q': 'qq.com', 'w': 'weibo.com', 'e': 'ele.me', 'r': 'mozilla.com', 't': 'opera.com', 'y': 'zhihu.com', 'u': 'tianya.com' }
+// 取出 localStorage 种的 zzz 对应的 hash
+var hashInLocalStorage = getLocalStorage('zzz')
+if (hashInLocalStorage) {
   hash = hashInLocalStorage
 }
 
-i = 0
-while (i < keys.length) {
-  div1 = document.createElement('div')
-  main.appendChild(div1)
-  j = 0
-  while (j < keys[i].length) {
-    kbd = document.createElement('kbd')
+//2.生成键盘
+//遍历 keys，生成 kbd 标签
+for (var i = 0; i < keys.length; i++) {
+  var div = tag('div')
+  main.appendChild(div)
+  for (var j = 0; j < keys[i].length; j++) {
+    var button = createButton(keys[i][j])
+
+    var img = createImg(hash[keys[i][j]])
+
+    var kbd = tag('kbd')
     kbd.textContent = keys[i][j]
-    button = document.createElement('button')
-    button.textContent = 'E'
-    button.id = keys[i][j]
-    img = document.createElement("img")
-    img.className = "fav-ico"
-    if (hash[keys[i][j]]) {
-      img.src = "https://www." + hash[keys[i][j]] + "/favicon.ico"
-    } else {
-      img.src = "./images/holder.png"
-    }
-    img.onerror = function(event) {
-      event.target.src = "./images/holder.png"
-    }
-    button.onclick = function(xzkjcnxlkcjlk) {
-      hashKey = xzkjcnxlkcjlk.target.id
-      img2 = xzkjcnxlkcjlk.target.previousSibling
-      hash[hashKey] = prompt('请输入要修改的网址')
-      localStorage.setItem('zzz', JSON.stringify(hash))
-      console.log(hash)
-    }
     kbd.appendChild(img)
     kbd.appendChild(button)
-    div1.appendChild(kbd)
-    j = j + 1
+
+    div.appendChild(kbd)
   }
-  i = i + 1
 }
 
+//3.监听键盘
 document.onkeypress = function (xzkjcnxlkcjlk) {
-  key = xzkjcnxlkcjlk['key']
-  address = hash[key]
+  var key = xzkjcnxlkcjlk['key']
+  var address = hash[key]
   // this.location.href = 'http://' + website
   window.open('http://' + address, '_blank')
+}
+function getLocalStorage(name) {
+  return JSON.parse(localStorage.getItem(name) || 'null')
+}
+function tag(tagName) {
+  return document.createElement(tagName)
+}
+function createButton(id) {
+  var button = tag('button')
+  button.textContent = 'E'
+  button.id = id
+  button.onclick = function (xzkjcnxlkcjlk) {
+    var hashKey = xzkjcnxlkcjlk.target.id
+    var img2 = xzkjcnxlkcjlk.target.previousSibling
+    hash[hashKey] = prompt('请输入要修改的网址')
+    localStorage.setItem('zzz', JSON.stringify(hash))
+    img2.src = "http://www." + hash[hashKey] + "/favicon.ico"
+    img2.onerror = function (event) {
+      event.target.src = "./images/holder.png"
+    }
+  }
+  return button
+}
+function createImg(domain) {
+  var img = tag("img")
+  img.className = "fav-ico"
+  if (domain) {
+    img.src = "https://www." + domain + "/favicon.ico"
+  } else {
+    img.src = "./images/holder.png"
+  }
+  img.onerror = function (event) {
+    event.target.src = "./images/holder.png"
+  }
+  return img
 }
