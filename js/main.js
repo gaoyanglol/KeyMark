e819cbab3b7f7cf931a9ced17c82e8af6dfa8582
gaoyanglol <gaoyanglol@hotmail.com>
2018-09-19 14:35:21 +0800

    js refactor

--- js/main.js
+++ js/main.js
@@ -1,45 +1,27 @@
 // 1.初始化数据
-var keys = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'], ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'], ['z', 'x', 'c', 'v', 'b', 'n', 'm']]
+var instance = init()
+var keys = instance['keys']
+var hash = instance['hash']
 
-var hash = { 'q': 'qq.com', 'w': 'weibo.com', 'e': 'ele.me', 'r': 'mozilla.com', 't': 'opera.com', 'y': 'zhihu.com', 'u': 'tianya.com' }
-// 取出 localStorage 种的 zzz 对应的 hash
-var hashInLocalStorage = getLocalStorage('zzz')
-if (hashInLocalStorage) {
-  hash = hashInLocalStorage
-}
 
 //2.生成键盘
 //遍历 keys，生成 kbd 标签
-for (var i = 0; i < keys.length; i++) {
-  var div = tag('div')
-  main.appendChild(div)
-  for (var j = 0; j < keys[i].length; j++) {
-    var button = createButton(keys[i][j])
+generateKbd(keys, hash)
 
-    var img = createImg(hash[keys[i][j]])
-
-    var kbd = tag('kbd')
-    kbd.textContent = keys[i][j]
-    kbd.appendChild(img)
-    kbd.appendChild(button)
-
-    div.appendChild(kbd)
-  }
-}
 
 //3.监听键盘
-document.onkeypress = function (xzkjcnxlkcjlk) {
-  var key = xzkjcnxlkcjlk['key']
-  var address = hash[key]
-  // this.location.href = 'http://' + website
-  window.open('http://' + address, '_blank')
-}
+listenToKbd(hash)
+
+
+//工具函数
 function getLocalStorage(name) {
   return JSON.parse(localStorage.getItem(name) || 'null')
 }
+
 function tag(tagName) {
   return document.createElement(tagName)
 }
+
 function createButton(id) {
   var button = tag('button')
   button.textContent = 'E'
@@ -56,6 +38,7 @@ function createButton(id) {
   }
   return button
 }
+
 function createImg(domain) {
   var img = tag("img")
   img.className = "fav-ico"
@@ -68,4 +51,47 @@ function createImg(domain) {
     event.target.src = "./images/holder.png"
   }
   return img
+}
+
+function init() {
+  var keys = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'], ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'], ['z', 'x', 'c', 'v', 'b', 'n', 'm']]
+
+  var hash = { 'q': 'qq.com', 'w': 'weibo.com', 'e': 'ele.me', 'r': 'mozilla.com', 't': 'opera.com', 'y': 'zhihu.com', 'u': 'tianya.com' }
+  // 取出 localStorage 种的 zzz 对应的 hash
+  var hashInLocalStorage = getLocalStorage('zzz')
+  if (hashInLocalStorage) {
+    hash = hashInLocalStorage
+  }
+  return {
+    "keys": keys,
+    "hash": hash
+  }
+}
+
+function generateKbd(keys, hash) {
+  for (var i = 0; i < keys.length; i++) {
+    var div = tag('div')
+    main.appendChild(div)
+    for (var j = 0; j < keys[i].length; j++) {
+      var button = createButton(keys[i][j])
+  
+      var img = createImg(hash[keys[i][j]])
+  
+      var kbd = tag('kbd')
+      kbd.textContent = keys[i][j]
+  
+      kbd.appendChild(img)
+      kbd.appendChild(button)
+      div.appendChild(kbd)
+    }
+  }
+}
+
+function listenToKbd(hash) {
+  document.onkeypress = function (xzkjcnxlkcjlk) {
+    var key = xzkjcnxlkcjlk['key']
+    var address = hash[key]
+    // this.location.href = 'http://' + website
+    window.open('http://' + address, '_blank')
+  }
 }
